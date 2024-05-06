a1643d9a7d754f259e01ec6d52765318e320f3f8
Steven Dickerson <poddocdan@gmail.com>
2024-05-06 11:30:57 -0700

    MYSQL submit

--- My SQL Intro.sql
+++ My SQL Intro.sql
@@ -13,7 +13,7 @@ SELECT * FROM Products WHERE Price IN (11.99, 13.99);
 SELECT * FROM Products WHERE Price != 11.99;
 
 -- find all products and sort them by price from greatest to least
-SELECT * FROM Products WHERE Price ORDER BY Price DESC;
+SELECT * FROM Products ORDER BY Price DESC;
 
 -- find all employees who don't have a middle initial
 SELECT * FROM employees WHERE MiddleInitial IS NULL OR MiddleInitial=''; 
@@ -34,7 +34,7 @@ SELECT * FROM products WHERE OnSale;
 SELECT Avg(price) FROM products;
 
 -- find all Geek Squad employees who don't have a middle initial
-SELECT * FROM employees WHERE MiddleInitial IS NULL OR MiddleInitial ='' AND EmployeeID = '%Geek Squad%';
+SELECT * FROM employees WHERE (MiddleInitial IS NULL OR MiddleInitial ='') AND Title = 'Geek Squad';
 
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest. Hint: Use the between keyword
 SELECT * FROM products WHERE StockLevel BETWEEN 500 AND 1200 ORDER BY price ASC;
